ece55b09998a07935d29f8de66e2ec06bad49304
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2023-07-23 17:13:38 +0500

    d

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,13 @@
 import Meta from "@/components/Meta";
 import { AuthContextProvider } from "@/context/AuthContext";
 import "@/styles/globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }) {
   return (
     <AuthContextProvider>
       <Meta />
       <Component {...pageProps} />;
+      <Analytics />
     </AuthContextProvider>
   );
 }
